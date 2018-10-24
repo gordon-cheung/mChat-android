177fac54ec87ab432b0ce067e8884e09ce490a5e
bryson189 <bryson189@gmail.com>
2018-10-24 13:23:53 -0600

    Creates basic UI

--- app/src/main/java/com/example/macbook/mchat/MainActivity.kt
+++ app/src/main/java/com/example/macbook/mchat/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.macbook.mchat
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val connectButton = findViewById<Button>(R.id.connectButton)
+        val connectStatusTextView = findViewById<TextView>(R.id.bluetoothStatus)
+
+        connectButton.setOnClickListener{
+            val rand = Random().nextInt(10)
+            if (rand < 5){
+                connectStatusTextView.text = "Disconnected"
+            }
+            else {
+                connectStatusTextView.text = "Connected"
+            }
+        }
     }
 }
