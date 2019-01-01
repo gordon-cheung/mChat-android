6ccebc86bd5429ef37d9a2e6495e057c2f152f1f
bryson189 <bryson189@gmail.com>
2018-12-31 20:30:54 -0700

    Completes basic RecyclerView template for contacts view

--- app/src/main/java/com/example/macbook/mchat/MainActivity.kt
+++ app/src/main/java/com/example/macbook/mchat/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.macbook.mchat
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,17 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val connectButton = findViewById<Button>(R.id.refreshButton)
-        val connectStatusTextView = findViewById<TextView>(R.id.bluetoothStatus)
+        val newMessageButton = findViewById<Button>(R.id.newMessageButton)
 
-        connectButton.setOnClickListener{
-            val rand = Random().nextInt(10)
-            if (rand < 5){
-                connectStatusTextView.text = "Disconnected"
-            }
-            else {
-                connectStatusTextView.text = "Connected"
-            }
+        newMessageButton.setOnClickListener{
+            val intent = Intent(this, SelectContactActivity::class.java)
+            startActivity(intent)
         }
 
     }
