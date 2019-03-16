3950131e99a0907a3bb990c3e56c042f92adcf80
Gordon Cheung <gordoncheung2@gmail.com>
2019-03-16 01:50:04 -0600

    Add bluetooth icon to app bar

--- app/src/main/java/com/example/macbook/mchat/MChatActivity.java
+++ app/src/main/java/com/example/macbook/mchat/MChatActivity.java
@@ -4,9 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 public abstract class MChatActivity extends AppCompatActivity {
     private String TAG = MChatActivity.class.getSimpleName();
@@ -36,6 +41,7 @@ public abstract class MChatActivity extends AppCompatActivity {
         return intentFilter;
     }
 
+    @Override
     protected void onResume() {
         super.onResume();
         registerReceiver(appNotificationReceiver, appNotificationIntentFilter());
@@ -51,6 +57,29 @@ public abstract class MChatActivity extends AppCompatActivity {
 
         // TODO does this mean it is not receiving messages when it is paused?
         unregisterReceiver(appNotificationReceiver);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_bluetooth:
+                Intent intent = new Intent(this, SelectDeviceActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     public abstract void onAppNotificationReceived(Intent intent);
