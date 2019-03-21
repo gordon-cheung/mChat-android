f34b719d60cca3fe4d4bd5142901b8abe16fe928
Gordon Cheung <gordoncheung2@gmail.com>
2019-03-21 00:14:14 -0600

    Chat Activity cleanup

--- app/src/main/java/com/example/macbook/mchat/ChatActivity.java
+++ app/src/main/java/com/example/macbook/mchat/ChatActivity.java
@@ -3,7 +3,6 @@ package com.example.macbook.mchat;
 import android.content.*;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.media.ThumbnailUtils;
 import android.net.Uri;
@@ -18,10 +17,6 @@ import android.util.Log;
 import android.view.Display;
 import android.widget.*;
 import android.view.View;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +25,8 @@ public class ChatActivity extends MChatActivity {
     private String TAG = ChatActivity.class.getSimpleName();
     private RecyclerView mRecyclerView;
     private ChatAdapter mAdapter;
+
+    // TODO Aggregate Contact to this class
     private String contactId;
     private ArrayList<String> mPictures = new ArrayList<String>();
 
@@ -46,37 +43,31 @@ public class ChatActivity extends MChatActivity {
         contactId = getIntent().getExtras().getString(AppNotification.CONTACT_DATA);
         getSupportActionBar().setTitle(contactId);
 
-        // TODO remove initial messageList
-        ArrayList<Message> messageList =  new ArrayList<Message>();
-
-        Display display = getWindowManager().getDefaultDisplay();
-        Point displaySize = new Point();
-        display.getSize(displaySize);
-
-        mRecyclerView = findViewById(R.id.reyclerview_message_list);
-        // TODO update user list
+        Point displaySize = getDisplaySize();
         mAdapter = new ChatAdapter(displaySize.x, displaySize.y);
 
+        mRecyclerView = findViewById(R.id.reyclerview_message_list);
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         new GetMessagesTask().execute();
 
+        bindClickEvents();
+    }
+
+    private void bindClickEvents() {
         final Button sendButton = findViewById(R.id.button_chatbox_send);
         sendButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 final EditText editText = findViewById(R.id.edittext_chatbox);
                 String message = editText.getText().toString();
 
-                // TODO use file path for images
                 for (String imageFilePath : mPictures) {
-                    sendPictureMessage(imageFilePath);
+                    final Message msg = new Message(imageFilePath, contactId, Message.IS_SEND, Message.PICTURE);
+                    sendMessage(msg);
                 }
 
-                mPictures.clear();
-                findViewById(R.id.layout_picture).setVisibility(View.GONE);
-                TextView pictureText = findViewById(R.id.textview_picture);
-                pictureText.setText(mPictures.size() + " images attached");
+                clearImages();
 
                 if (!message.isEmpty()) {
                     sendMessage(new Message(message, contactId, Message.IS_SEND, Message.TEXT));
@@ -88,10 +79,7 @@ public class ChatActivity extends MChatActivity {
         final ImageButton pictureButton = findViewById(R.id.button_picture);
         pictureButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-//                intent.setType("image/*");
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(galleryIntent, GALLERY);
             }
         });
@@ -99,12 +87,7 @@ public class ChatActivity extends MChatActivity {
         final AppCompatImageButton removePicButton = findViewById(R.id.button_picture_remove);
         removePicButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mPictures.clear();
-
-                LinearLayout layout = findViewById(R.id.layout_picture);
-                layout.setVisibility(View.GONE);
-                TextView pictureText = findViewById(R.id.textview_picture);
-                pictureText.setText("");
+                clearImages();
             }
         });
 
@@ -124,33 +107,23 @@ public class ChatActivity extends MChatActivity {
                 sendBroadcast(broadcastTestIntent);
             }
         });
+    }
 
-//        try {
-//            Display display = getWindowManager().getDefaultDisplay();
-//            Point size = new Point();
-//            display.getSize(size);
-//            ImageView imageView = (ImageView) findViewById(R.id.picture_message_body);
-//            Uri mUri = Uri.fromFile(new File("/storage/emulated/0/DCIM/Camera/20190318_224056.jpg"));
-//            Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), mUri);
-//            bitmap = getThumbnail(bitmap, size.x / 2, size.y / 2);
-//            int byteCount = bitmap.getByteCount();
-//            imageView.setImageBitmap(bitmap);
-//            imageView.setOnClickListener(new View.OnClickListener() {
-//                public void onClick(View v) {
-//                    Log.d(TAG, "TEST BUTTON CLICKED");
-//
-//                    // Enable this to test startNetworkRegistration
-//                    //mBluetoothService.startNetworkRegistration();
-//                    Uri mUri = Uri.fromFile(new File("/storage/emulated/0/DCIM/Camera/20190318_224056.jpg"));
-//                    // Enable this to test broadcast notification
-//                    Intent intent = new Intent(getApplicationContext(), FullImageViewActivity.class);
-//                    startActivity(intent);
-//                }
-//            });
-//        } catch(Exception ex) {
-//            Log.d(TAG, ex.getMessage());
-//        }
+    private void clearImages() {
+        mPictures.clear();
 
+        LinearLayout layout = findViewById(R.id.layout_picture);
+        layout.setVisibility(View.GONE);
+        TextView pictureText = findViewById(R.id.textview_picture);
+        pictureText.setText("");
+    }
+
+    private Point getDisplaySize() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point displaySize = new Point();
+        display.getSize(displaySize);
+
+        return displaySize;
     }
 
     private boolean sendMessage(final Message msg) {
@@ -176,36 +149,6 @@ public class ChatActivity extends MChatActivity {
         } else {
             Log.e(TAG, "Message failed to send");
         }
-        return true;
-    }
-
-    private boolean sendPictureMessage(final String imageFilePath) {
-        int currentSize = mAdapter.getItemCount();
-
-        // TODO show image as view
-        // How to do this and make it an intent to show image when clicked
-        final Message msg = new Message(imageFilePath, contactId, Message.IS_SEND, Message.PICTURE);
-        // Update UI
-        mAdapter.addMessage(msg);
-        mAdapter.notifyItemInserted(currentSize);
-        mRecyclerView.scrollToPosition(mAdapter.getItemCount() - 1);
-
-        // TODO store image correctly (use file path)
-//        AsyncTask.execute(new Runnable() {
-//            @Override
-//            public void run() {
-//                Log.d(TAG, "Inserting stored sent message");
-//                AppDatabase.getInstance().messageDao().insert(msg);
-//            }
-//        });
-//
-//        if (mBluetoothService.send(msg)) {
-//            Log.d(TAG, "Message successfully sent");
-//        } else {
-//            Log.e(TAG, "Message failed to send");
-//            return false;
-//        }
-
         return true;
     }
 
@@ -250,55 +193,26 @@ public class ChatActivity extends MChatActivity {
         if (requestCode == GALLERY) {
             if (data != null) {
                 Uri contentURI = data.getData();
-                try {
-                    String filePath = getPath(contentURI);
-                    // TODO Store the path instead?
-                    //Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
-                    mPictures.add(filePath);
-                    findViewById(R.id.layout_picture).setVisibility(View.VISIBLE);
+                String filePath = getPath(contentURI);
+                mPictures.add(filePath);
+                findViewById(R.id.layout_picture).setVisibility(View.VISIBLE);
 
-                    TextView pictureText = findViewById(R.id.textview_picture);
-                    if (mPictures.size() > 1) {
-                        pictureText.setText(mPictures.size() + " images attached");
-                    }
-                    else {
-                        pictureText.setText(mPictures.size() + " image attached");
-                    }
-                    pictureText.setText(mPictures.size() + " image attached");
-
-                } catch (Exception e) { //IOException
-                    Log.e(TAG, "Error getting image");
+                TextView pictureText = findViewById(R.id.textview_picture);
+                if (mPictures.size() > 1) {
+                    pictureText.setText(mPictures.size() + " images attached");
                 }
+                else {
+                    pictureText.setText(mPictures.size() + " image attached");
+                }
+                pictureText.setText(mPictures.size() + " image attached");
             }
-
         }
     }
 
-    private Bitmap getThumbnail(Bitmap image, float maxWidth, float maxHeight) {
-        float width = image.getWidth();
-        float height = image.getHeight();
-        float ratio = width/height;
-
-        if (width >= height) {
-            if (width > maxWidth) {
-                width = maxWidth;
-                height = maxWidth / ratio;
-            }
-        }
-        else {
-            if (height > maxHeight) {
-                height = maxHeight;
-                width = maxHeight * ratio;
-            }
-        }
-
-        return ThumbnailUtils.extractThumbnail(image, (int)width, (int)height);
-    }
-
+    // TODO use non-deprecated function
     private String getPath(Uri uri) {
-        // just some safety built in
         if( uri == null ) {
-            // TODO perform some logging or show user feedback
+            Log.e(TAG, "Null URI");
             return null;
         }
         // try to retrieve the image from the media store first
@@ -313,7 +227,7 @@ public class ChatActivity extends MChatActivity {
             cursor.close();
             return path;
         }
-        // this is our fallback here
+
         return uri.getPath();
     }
 
